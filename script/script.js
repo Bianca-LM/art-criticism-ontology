ab869376fda804b108420d5189419dadb190d6b8
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2023-05-26 11:12:18 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,24 @@
 function showResult(element) {
-    element.parentNode.style.backgroundColor = "rgb(236, 65, 8)"
-    //element.previousSibling.setAttribute("class", "hide")
+    element.style.backgroundColor = "rgb(236, 65, 8)"
+    id = element.getAttribute('id')
+    if (id == 'query-1') {
+        result = document.getElementById('result-1')
+        result.removeAttribute('class')
+    }
+    if (id == 'query-2') {
+        result = document.getElementById('result-2')
+        result.removeAttribute('class')
+    }
+    if (id == 'query-3') {
+        result = document.getElementById('result-3')
+        result.removeAttribute('class')
+    }
+    if (id == 'query-4') {
+        result = document.getElementById('result-4')
+        result.removeAttribute('class')
+    }
+    if (id == 'query-5') {
+        result = document.getElementById('result-5')
+        result.removeAttribute('class')
+    }
 }
