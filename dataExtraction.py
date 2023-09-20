e410eda4294d18df9ecefc0c6a7558f3ad87b285
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2023-09-20 15:09:02 +0200

    Update dataExtraction.py

--- dataExtraction.py
+++ dataExtraction.py
@@ -8,27 +8,17 @@ from sparql_dataframe import get
 from SPARQLWrapper import *
 
 
+#this function perform a sparql query on an endpoint and return it into JSON format
 def askSkosVocabulary(endpoint, query):
-
     client = SPARQLWrapper(endpoint)
     client.setQuery(query)
     client.setReturnFormat(JSON)
     results = client.query().convert()
-    finalResult = vocabulary(results)
+    finalResult = vocabularyTranslation(results)
     return finalResult
 
-def correctTranslation(data):
-    if "tin" in data: 
-        data.remove("tin") #tin and lands create ambiguation (since painting)
-        data.remove("blue") #no clue why it is on the list, thanks to Google translator
-        data.append("metal sheet") #for "latta"
-    if "lands" in data: 
-        data.remove("lands")
-        data.extend(["oil", "tempera"])
-        data.append("earth") #for "terre"
-    return data
-
-def vocabulary(jsonQueryResults): 
+#automatic translation of the vocabulary from a JSON file returned by a SPARQL query
+def vocabularyTranslation(jsonQueryResults): 
     data = set()
     enData = set()
     for i in jsonQueryResults["results"]["bindings"]:
@@ -38,6 +28,20 @@ def vocabulary(jsonQueryResults):
     translatedData = correctTranslation(list(enData))
     return translatedData
 
+#specific correction of the automatic tranlation
+def correctTranslation(data):
+    if "tin" in data: #this means it is the material vocabulary
+        data.remove("tin") #tin and lands create ambiguation (since painting)
+        data.remove("blue") 
+        data.append("metal sheet") #for "latta"
+    if "lands" in data: #this means is the technique vocabulary
+        data.remove("lands")
+        data.extend(["oil", "tempera"])
+        data.append("earth") #for "terre"
+    return data
+
+
+#automatic extraction of data: it takes in input the directory of the .txt file where the information must be extracted and the two SKOS vocabulary
 def dataExtraction(directory, materials, techniques):
 
     allExtractedData = {}
@@ -52,10 +56,11 @@ def dataExtraction(directory, materials, techniques):
 
         txt = []
 
-        idx = 000
+        idx = 000 #the index we will give to each resource
         for paragraph in re.split(r'\n?\n\n\n', text):
             txt.append(paragraph)
             txt.append("PARAGRAPH END")
+            #the first if finds a titles which contain the name of the artist
             if re.match(r'\n*[a-zA-Z ]+\n(?!\d)', paragraph): 
                 processedParagraph = nlp(paragraph)
                 if processedParagraph.ents[0].label_ == "PERSON" and processedParagraph.ents[0].text != False:
@@ -64,10 +69,13 @@ def dataExtraction(directory, materials, techniques):
                         author = re.sub(r'\n.+', '', author)
                         author = re.sub(r'\n', '', author) 
                     authorName = author
+            #this if finds the titles which contain name of workshop of artists
             elif re.search(r'\n*Workshop of [a-zA-Z ]+(?!\d)', paragraph):
                 workshopOfAuthor = "Workshop of "+authorName
                 author = workshopOfAuthor
+            
             elif re.match(r"\n*.+\n\d+", paragraph):
+                #this block finds the titles which contains the name of the artwork and assign it to the artist
                 title = re.search("\n*.+\n", paragraph).group() 
                 if '\n' in title: 
                     title = re.sub(r'\n', '', title)
@@ -75,13 +83,15 @@ def dataExtraction(directory, materials, techniques):
                     allExtractedData["P"+str("{:03d}".format(idx))] = {"author": author, "title": title}
                 else: 
                     allExtractedData["P"+str("{:03d}".format(idx))] = {"author": author, "title": title}
-
+                
+                #automatic extraction of materials from the paragraph
                 usedMaterials = []
                 for material in materials: 
                     findMaterial = re.findall(r'(?<![a-zA-Z])'+material+r'(?![a-zA-Z])', paragraph, flags=re.IGNORECASE)
                     usedMaterials.extend(findMaterial)
                 allExtractedData["P"+str("{:03d}".format(idx))]["material"] = list(set(usedMaterials))
                 
+                #automatic extraction of techniques from the paragraph
                 usedTechniques = []
                 for technique in techniques: 
                     findTechniques = re.findall(r'(?<![a-zA-Z])'+technique+r'(?![a-zA-Z])', paragraph, flags=re.IGNORECASE)
@@ -89,7 +99,8 @@ def dataExtraction(directory, materials, techniques):
                 allExtractedData["P"+str("{:03d}".format(idx))]["technique"] = list(set(usedTechniques))
 
                 idx+=1
-                
+
+    #from JSON to CSV format          
     identifier = []
     author = []
     title = []
@@ -109,6 +120,7 @@ def dataExtraction(directory, materials, techniques):
 
 
 
+#endpoint where to perform the sparql queries to ask for the vocabularies
 endpoint = 'https://dati.cultura.gov.it/sparql'
 
 queryMaterials = """
